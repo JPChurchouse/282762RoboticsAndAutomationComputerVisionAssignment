4f6f2bb1e4245c878ec660193abfd92ea99c26e7
JPChurchouse <116853805+JPChurchouse@users.noreply.github.com>
2023-05-10 21:31:17 +1200

    wip, new hsv testing programme

--- Application/Application.py
+++ Application/Application.py
@@ -4,6 +4,30 @@ import numpy as np
 import os
 import sys
 
+# remove white
+hsv_low_white = np.array([0,52,0])
+hsv_high_white = np.array([255,255,255])
+
+# 
+
+
+
+# Function to process images
+def process_image(image_input):
+    grey = cv2.cvtColor(image_input, cv2.COLOR_BGR2GRAY)
+    #blur = cv2.GaussianBlur(grey, (11, 11), 0)
+    circles	= cv2.HoughCircles(grey,cv2.HOUGH_GRADIENT,1,120,param1=100,param2=30,minRadius=0,maxRadius=0)
+    circles	= np.uint16(np.around(circles))
+
+    for i in circles[0,:]:
+        cv2.circle(image_input, (i[0], i[1]), i[2], (0,255,0), 2)
+        
+    
+    
+    return image_input
+
+
+
 
 def main():
 
@@ -12,8 +36,7 @@ def main():
     directory_output = os.getcwd() + "/Output"
 
     ## Throw error if input directory does not exist
-    if not os.path.exists(directory_input):
-        return 1
+    assert os.path.exists(directory_input), "Path error"
 
     ## Create output directory if it does not exist
     if not os.path.exists(directory_output):
@@ -25,21 +48,20 @@ def main():
 
         # Generate local file variables
         targ_path_in = os.path.join(directory_input, targ_name_in)
+        targ_name_out = targ_name_in
+        targ_path_out = os.path.join(directory_output, targ_name_out)
+        
+        # Read image in
         targ_image_in = cv2.imread(targ_path_in, cv2.IMREAD_COLOR)
 
         # Test that image exists
-        if (targ_image_in is None):
-            print(f'Error: Image not found')
-            return 1
+        assert targ_image_in is not None, "Image not found"
         
         # Process image
-        targ_image_out = process_image(targ_path_in)
+        targ_image_out = process_image(targ_image_in)
 
         # Save and display
-        targ_name_out = targ_name_in
-        targ_path_out = os.path.join(directory_output, targ_name_out)
         cv2.imwrite(targ_path_out, targ_image_out)
-        
         cv2.imshow(f"{targ_name_out}", targ_image_out)
 
 
@@ -50,23 +72,3 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
-
-
-# Function to process images
-def process_image(image_input):
-    num_apples = 0
-
-    ###
-    range_low = np.array([1,2,3])
-    range_hig = np.array([1,2,3])
-
-
-    ###
-
-    cv2.rectangle(image_hsv, (0,0), (32, 18), (255,255,255), -1)#create rectangle in corner
-    cv2.putText(image_hsv, f"{num_apples}", (1,1), cv2.FONT_HERSHEY_COMPLEX, 0.2, (0,0,0),1,1)
-    image_hsv = cv2.cvtColor(image_input, cv2.COLOR_BGR2HSV)
-    return image_hsv
-
-
-
