3f0e484414a2acd16fcf350f272d1c051cb1315d
JPChurchouse <116853805+JPChurchouse@users.noreply.github.com>
2023-05-12 22:56:15 +1200

    comments, file naming update

--- Application/Application.py
+++ Application/Application.py
@@ -4,60 +4,51 @@
 # https://codeloop.org/python-opencv-circle-detection-with-houghcircles/
 # https://www.youtube.com/watch?v=8CMTqpZoec8
 # https://stackoverflow.com/questions/58353513/detecting-apple-by-thresholding
-# 
-
 
 import cv2 
 import numpy as np
-
 import os
 import sys
-
-
 
 # Function to process images
 def process_image(img_in):
 
-    # Blur image slightly to reduce noise
+    ###  CREATE MASK  ###
+
+    # Blur image and convert to HSV
     ker_siz = 49
-    mask_blur = cv2.GaussianBlur(img_in, (ker_siz, ker_siz), 0)
+    img_blr = cv2.GaussianBlur(img_in, (ker_siz, ker_siz), 0)
+    img_hsv = cv2.cvtColor(img_blr, cv2.COLOR_BGR2HSV)
 
-    # Convert to HSV for processing
-    img_hsv = cv2.cvtColor(mask_blur, cv2.COLOR_BGR2HSV)
-    
-    
-
-    # HSV boundaries
+    # Define HSV boundaries
     s_min = 60.0
     v_min = 20.0
     h_max = 80.0
     h_min = 160.0
 
-    # lower boundary RED color range values; Hue (0 - 10)
-    hsv_A_lo = np.array([  0.0, s_min, v_min]) # Cutoff white
-    hsv_A_hi = np.array([h_max, 255.0, 255.0]) # Pure red -> green/
-    
-    # upper boundary RED color range values; Hue (160 - 180)
-    hsv_B_lo = np.array([h_min, s_min, v_min]) # Cutoff white
-    hsv_B_hi = np.array([180.0, 255.0, 255.0]) # Magenta/ -> Red
+    # Params for H = MIN -> X
+    hsv_A_lo = np.array([  0.0, s_min, v_min])
+    hsv_A_hi = np.array([h_max, 255.0, 255.0])
+    # Params for H = X -> MAX
+    hsv_B_lo = np.array([h_min, s_min, v_min])
+    hsv_B_hi = np.array([180.0, 255.0, 255.0])
 
-    # Run maskwork
+    # Generate masks based on those params and combine
     mask_A = cv2.inRange(img_hsv, hsv_A_lo, hsv_A_hi)
     mask_B = cv2.inRange(img_hsv, hsv_B_lo, hsv_B_hi)
-    
-    # combine to make final mask
-    mask_final = mask_A + mask_B
+    mask_fin = mask_A + mask_B
 
+    ### APPLE DETECTION ###
 
-
-    # dilation
+    # Dialation and Erosion
     ker_siz = 7
     kernel = np.ones((ker_siz,ker_siz), np.uint8)
     #mask_open = cv2.morphologyEx(mask_final, cv2.MORPH_CLOSE, kernel, iterations=3)
-    mask_open = cv2.morphologyEx(mask_final, cv2.MORPH_OPEN, kernel, iterations=3)
+    mask_open = cv2.morphologyEx(mask_fin, cv2.MORPH_OPEN, kernel, iterations=3)
     #mask_open = cv2.erode(mask_final, kernel, iterations=3)
     #mask_open	= cv2.medianBlur(mask_open,	3)
     
+    # Detect circular shapes with HoughCircles
     detections	= cv2.HoughCircles(mask_open,cv2.HOUGH_GRADIENT,
                                dp=1.3,
                                minDist=100, 
@@ -66,71 +57,78 @@ def process_image(img_in):
                                minRadius=60,
                                maxRadius=130
                                )
-    
-    if detections is None: return img_in
-    detections	= np.uint16(np.around(detections))
 
+    # No apples detetced
+    if detections is None: return img_hsv
+
+    # Pre made colours
     col_G = (0,255,0)
     col_W = (255,255,255)
     col_B = (0,0,0)
-    
+
+    # Setup for next phase
+    detections	= np.uint16(np.around(detections))
     img_out = img_in#cv2.cvtColor(mask_open, cv2.COLOR_GRAY2BGR)
+    num_apl = len(detections[0,:])
+
+    # Highlight each apple
     for	index, apples in enumerate(detections[0,:], start=1):
-        #	draw	the	outer	circle
-        cv2.circle(img_out,(apples[0],apples[1]),apples[2],col_G,6)
-        #	draw	the	center	of	the	circle
-        cv2.circle(img_out,(apples[0],apples[1]),2,col_G,4)
-        cv2.putText(img_out,f"{index}",(apples[0]-20,apples[1]-15),cv2.FONT_HERSHEY_SIMPLEX,1.5,col_W,2)
-    
-    # Print a total on the image
-    cv2.rectangle(img_out, (0,0), (300,40),col_B,cv2.FILLED)
-    cv2.putText(img_out,f"Total apples: {len(detections[0,:])}",(1,30),cv2.FONT_HERSHEY_SIMPLEX,1,col_W,2)
-    
-    return img_out
+        cv2.circle(img_out,(apples[0],apples[1]),apples[2],col_G,6) # Draw circle around target
+        cv2.circle(img_out,(apples[0],apples[1]),2,col_G,4)         # Draw dot in centre of traget
+        cv2.putText(img_out,f"{index}",(apples[0]-20,apples[1]-15), # Put target ID in center of target
+                    cv2.FONT_HERSHEY_SIMPLEX,1.5,col_W,2)
 
+    # Print a total count on the image
+    cv2.rectangle(img_out, (0,0), (300,40),col_B,cv2.FILLED)        # Background of count
+    cv2.putText(img_out,f"Total apples: {num_apl}",(1,30),          # Write total count
+                cv2.FONT_HERSHEY_SIMPLEX,1,col_W,2)
 
+    return img_out, num_apl
 
+# Main function
 def main():
+
+    # Advise the user that the programme has begun
     print("BEGIN")
 
-    ## Declare input and output directories
+    # Declare input and output directories
     directory_input = os.getcwd() + "/Resources"
     directory_output = os.getcwd() + "/Output"
 
-    ## Throw error if input directory does not exist
+    # Throw error if input directory does not exist
     assert os.path.exists(directory_input), "Path error"
 
-    ## Create output directory if it does not exist
-    if not os.path.exists(directory_output):
-        os.makedirs(directory_output)
+    # Create output directory if it does not exist
+    if not os.path.exists(directory_output): os.makedirs(directory_output)
 
 
     # Run for each image in the input directory
     for targ_name_in in os.listdir(directory_input):
 
-        # Generate local file variables
-        targ_path_in = os.path.join(directory_input, targ_name_in)
-        targ_name_out = targ_name_in
-        targ_path_out = os.path.join(directory_output, targ_name_out)
-        
         # Read image in
+        targ_path_in = os.path.join(directory_input, targ_name_in)
         targ_image_in = cv2.imread(targ_path_in, cv2.IMREAD_COLOR)
 
         # Test that image exists
         assert targ_image_in is not None, "Image not found"
         
         # Process image
-        targ_image_out = process_image(targ_image_in)
+        targ_image_out, num_apples = process_image(targ_image_in)
 
         # Save and display
+        targ_name_out = f"{num_apples}_" + targ_name_in
+        targ_path_out = os.path.join(directory_output, targ_name_out)
         cv2.imwrite(targ_path_out, targ_image_out)
         #cv2.imshow(f"{targ_name_out}", targ_image_out)
 
+    # Advise user processing has completed
     print("DONE")
+
     # Destroy all images at end
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
